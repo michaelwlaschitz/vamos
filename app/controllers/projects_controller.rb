a7df560466e08c3ce30153bb4b5c76962bd2f804
Joyce Cakmak <joyce.cakmak@gmail.com>
2020-03-24 17:25:47 +0100

    part1

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,10 +2,24 @@ class ProjectsController < ApplicationController
 
   def index
     @projects = Project.all
+
+    @projects = Project.geocoded #returns project with coordinates
+
+    @markers = @projects.map do |project|
+      {
+        lat: project.latitude,
+        lng: project.longitude
+      }
+    end
   end
 
   def show
     @project = Project.find(params[:id])
+
+      @markers = [{
+      lat: @project.latitude,
+      lng: @project.longitude
+    }]
   end
 
   def create
