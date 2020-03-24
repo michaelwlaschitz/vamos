568aa771714cf42833c36a9a906a6fb3d8e2a445
Laura Helmert <laura.helmert.berlin@gmail.com>
2020-03-24 16:11:17 +0100

    Validations and Assocations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,13 @@
 class Project < ApplicationRecord
-  belongs_to :ngo
+  has_one :ngo
+  has_many :bookings
   has_many_attached :photos
+
+  validates :title, length: { maximum: 25 }, uniqueness: true, presence: true
+  validates :hours_per_week, presence: true
+  validates :address, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :capacity, presence: true
+  validates :category, presence: true
+
 end
--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -1,4 +1,5 @@
 class TeamMembership < ApplicationRecord
   belongs_to :user
   belongs_to :team
+
 end
