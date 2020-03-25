d70529bda0a99ceb139c847f1e33be4a9235bf02
Elena Vigo <elenavigoolivan@gmail.com>
2020-03-25 15:02:59 +0100

    location filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
+
+  end
+
+  def filter_location
+    @project = Project.new
+  end
+
+  def filter_interests
+
+  end
+
+  def filter_time
+
   end
 end
