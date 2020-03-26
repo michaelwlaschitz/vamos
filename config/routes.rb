73f02006591bdcc8d1dbf05d14f15ffba15cfa13
Laura Helmert <laura.helmert.berlin@gmail.com>
2020-03-26 18:38:52 +0100

    backend bookings and teamcreation

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
    end
 
-
-
+  resources :teams, only: [:create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
