4940c6610c82c26a36dd8b111c5ff65a56bebee2
Joyce Cakmak <joyce.cakmak@gmail.com>
2020-03-26 18:04:27 +0100

    index style and calculating

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,10 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapModal } from "../functions/map_modal.js"
 import { initMapbox } from '../plugins/init_mapbox';
+import { replaceDistances } from '../functions/distance';
 
 
 
 initMapbox();
 initMapModal();
+replaceDistances();
