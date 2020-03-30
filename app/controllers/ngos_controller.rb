bc49d587da798b636eeac2234b2ae91d1a3e7bc0
Michael Wlaschitz <michael.wlaschitz@gmail.com>
2020-03-30 10:56:12 +0200

    first form codes

--- app/controllers/ngos_controller.rb
+++ app/controllers/ngos_controller.rb
@@ -1,2 +1,22 @@
 class NgosController < ApplicationController
+
+  def new
+    @ngo = Ngo.new
+  end
+
+  def create
+    @ngo = Ngo.new(ngo_params)
+    @ngo.user = current.user
+    if @ngo.save
+      redirect_to ngo_dashboard(@ngo)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def ngo_params
+    params.require(:ngo).permit(:name, :phone, :description)
+  end
 end
