36b4b4fa45bb268308d7cdcc184987e83c4c9c16
Elena Vigo <elenavigoolivan@gmail.com>
2020-04-14 11:32:48 +0200

    FUCKING MAP IS FIXED

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,10 +10,11 @@ const fitMapToMarkers = (map, markers) => {
 
 
 const initMapbox = () => {
-  const mapElements = document.querySelectorAll('#map');
+  const mapMobile = document.getElementById('map-mobile');
+  const mapDesktop = document.getElementById('map-desktop')
+  const mapElement = document.getElementById('map')
 
-  if (mapElements) { // only build a map if there's a div#map to inject into
-    mapElements.forEach((mapElement) => {
+    if (mapElement) { // only build a map if there's a div#map to inject into
 
       mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
       const map = new mapboxgl.Map({
@@ -41,9 +42,65 @@ const initMapbox = () => {
         map.resize()
         console.log("test");
       });
+    }
 
-    });
-  }
+    if (mapDesktop) { // only build a map if there's a div#map to inject into
+
+      mapboxgl.accessToken = mapDesktop.dataset.mapboxApiKey;
+      const map = new mapboxgl.Map({
+        container: 'map-desktop',
+        style: 'mapbox://styles/mapbox/streets-v10',
+        center: [-74.5, 40], // starting position
+        zoom: 9 // starting zoom
+      });
+
+      map.addControl(new mapboxgl.NavigationControl());
+
+
+      const markers = JSON.parse(mapDesktop.dataset.markers);
+      markers.forEach((marker) => {
+        const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+        new mapboxgl.Marker()
+          .setLngLat([ marker.lng, marker.lat ])
+          .setPopup(popup) // add this
+          .addTo(map);
+      });
+
+      fitMapToMarkers(map, markers);
+    }
+
+    if (mapMobile) { // only build a map if there's a div#map to inject into
+
+      mapboxgl.accessToken = mapMobile.dataset.mapboxApiKey;
+      const map = new mapboxgl.Map({
+        container: 'map-mobile',
+        style: 'mapbox://styles/mapbox/streets-v10',
+        center: [-74.5, 40], // starting position
+        zoom: 9 // starting zoom
+      });
+
+      map.addControl(new mapboxgl.NavigationControl());
+
+
+      const markers = JSON.parse(mapMobile.dataset.markers);
+      markers.forEach((marker) => {
+        const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+        new mapboxgl.Marker()
+          .setLngLat([ marker.lng, marker.lat ])
+          .setPopup(popup) // add this
+          .addTo(map);
+      });
+
+      fitMapToMarkers(map, markers);
+
+      $('#exampleModal').on('show.bs.modal', function () {
+        map.resize()
+        console.log("test");
+      });
+    }
+
+
+
 };
 
 export { initMapbox };
