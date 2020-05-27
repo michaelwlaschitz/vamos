8b462a7272c6080728db8a2951e2e0bbd6406cd1
Juan Estrella <juan.estrella137@gmail.com>
2020-05-27 17:09:38 +0200

    Fix routes to allow omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: {
+        sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 
   scope "(:locale)", locale: /es|de/ do
     root to: 'pages#home'
