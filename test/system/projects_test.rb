d7b5c44fbe9dd9dcd8d29150a78d0b09d9bb20fa
Michael Wlaschitz <michael.wlaschitz@gmail.com>
2020-06-08 15:19:13 +0200

    done

--- test/system/projects_test.rb
+++ test/system/projects_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class ProjectsTest < ApplicationSystemTestCase
 
   test "visiting the index" do
-  visit projects_url
-  assert_response :success
+    visit projects_url
+    assert_selector "h1", text: "xyz"
   end
 end
