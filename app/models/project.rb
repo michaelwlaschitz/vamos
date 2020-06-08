e894a78648253edf1e0f9e96881606ae89527c00
Joyce Cakmak <joyce.cakmak@gmail.com>
2020-06-08 19:07:41 +0200

    almost done

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,9 +4,9 @@ class Project < ApplicationRecord
   has_many :reviews, through: :applications
   has_many_attached :photos
 
-  validates :title, length: { maximum: 40 }, uniqueness: true, presence: true
+  validates :title, length: { maximum: 40 }, presence: true
   validates :start_date, presence: true
-  validates :address, presence: true, uniqueness: true
+  validates :address, presence: true
   validates :description, presence: true
   validates :capacity, presence: true
   validates :category, presence: true
